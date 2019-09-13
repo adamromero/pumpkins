238fb76e3069b7d5e54b9f8ae73588b7e288a107
Adam Romero <adamromero89@gmail.com>
2019-09-13 11:47:47 -0700

    Switch to using webpack instead of react scripts

---
+++ client/webpack.config.js
@@ -0,0 +1,54 @@
+const webpack = require('webpack');
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const autoprefixer = require('autoprefixer');
+
+module.exports = {
+  entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.(css|sass|scss)$/,
+        use: [
+          'style-loader',
+          { loader: 'css-loader', options: { sourceMap: true, importLoaders: 1 } },
+          { loader: 'sass-loader', options: { sourceMap: true } },
+        ],
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  output: {
+    path: __dirname + '/public',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: './public',
+    historyApiFallback: true,
+    hot: true
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "index.css",
+      chunkFilename: "[id].css"
+    }),
+    new OptimizeCssAssetsPlugin({
+      assetNameRegxp: /\.css$/,
+      cssProcessor: require('cssnano'),
+      cssProcessorPluginOptions: {
+        preset: ['default', { discardComments: { removeAll: true } }],
+      },
+      canPrint: true
+    }),
+    new webpack.HotModuleReplacementPlugin()
+  ]
+};
